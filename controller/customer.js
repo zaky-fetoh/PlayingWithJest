4ae21ccb35edc872f62dc4e9c08159a3c2ca0254
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-03 14:11:26 +0200

    customer update is added to controller

--- controller/customer.js
+++ controller/customer.js
@@ -17,3 +17,20 @@ exports.addCustomer = async (req, res, next) => {
     });
 };
 
+// PUT /customer
+exports.updateCustomer = async(req, res, next)=>{
+    let emsg = undefined;
+    const body = req.body; 
+    const custId = req.params.custId; 
+    const imDoc = await custModel.findOne({
+        _id: custId,},{__v:0});
+    body._id = undefined; 
+    Object.assign(imDoc, body); 
+    try{ await imDoc.save();}
+    catch(e){emsg = e.message;}
+    res.status(emsg? 500: 200).json({
+        ok: Boolean(emsg), 
+        message: emsg? emsg:"document updated", 
+        doc: emsg? undefined: imDoc._doc,
+    });
+}
