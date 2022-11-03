663621560d9d92636d6bce8398366c850c605a25
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-03 15:41:13 +0200

    addCustomer NormalBehavoir tasting is added

--- controller/customer.test.js
+++ controller/customer.test.js
@@ -23,8 +23,27 @@ describe("testing Contrller middleware", () => {
         addCustomer(reqMock, resMock);
     });
 
-    test("addCustomer function Error Behaviour",()=>{
-
+    test("addCustomer function Error Behaviour", () => {
+        custModelMock = jest.doMock("../model/customer", () => ({
+            create: jest.fn(async()=>{
+                throw "Some Error massage";
+            }),
+        }));
+        const reqMock = {
+            body: { name: "DAs", },
+        };
+        const resMock = {
+            status: jest.fn((x) => {
+                expect(x).toBe(500)
+                return resMock
+            }),
+            json: jest.fn((x) => {
+                expect(x.massage).toBe("Some Error massage");
+                expect(x.ok).toBe(false);
+                console.log(x.customer_id)
+            }),
+        }
+        const { addCustomer } = require("./customer");
+        addCustomer(reqMock, resMock);
     })
-
 })
