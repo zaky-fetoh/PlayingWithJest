c26d4c274b4cd0839e3d3a704658e4cd43150122
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-04 14:04:22 +0200

    behavoiural testing of app is added

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const custRoute = require("./routes/customer");
-const DB_URI = "mongodb://localhost:27017/JestDataTest";
+const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/JestDataTest";
 const PORT = process.env.PORT || 3000;
 
 (async () => {
     try {
         await mongoose.connect(DB_URI);
-        express().use(express.json())
+        express.call()
+            .use(express.json())
             .use(express.urlencoded())
             .use("/customer", custRoute)
             .listen(PORT, () => {
@@ -16,7 +17,7 @@ const PORT = process.env.PORT || 3000;
             })
     } catch (e) {
         console.log(e.message);
-        process.exit();
+        // process.exit();
     }
 })()
 
--- app.test.js
+++ app.test.js
@@ -1,3 +1,43 @@
-describe("functionalinteraction testing",()=>{
-    
+describe("functionalinteraction testing", () => {
+    const env = process.env;
+    beforeAll(() => {
+
+        process.env = {
+            PORT: 4000,
+            DB_URI: "hello",
+        }
+
+    })
+    afterAll(() => {
+        process.env = env
+    })
+
+
+    test("main Functionalinteraction with the main method", () => {
+        jest.resetModules();
+        ///Mocking the Express Module
+        let listenMock = jest.fn((x,f)=>{
+            expect(x).toBe(4000);
+            f();return app})
+        const app ={
+            use: jest.fn(() => app),
+            listen: listenMock,
+        }
+        const expressMock = {
+            call: () => app,
+            json: jest.fn(),
+            urlencoded: jest.fn(),
+        }
+        jest.doMock("express", () => expressMock);
+
+        //Mocking mongoose
+        jest.doMock("mongoose", () => ({
+            connect: jest.fn(x => {
+                expect(x).toBe("hello");
+            })
+        }))
+        //mocking Schema modules
+        jest.doMock("./routes/customer", () => ({}))
+        require("./app");
+    })
 })
